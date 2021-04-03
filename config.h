cb67e9c90213c177bc54a609c774ac1ec3b4f653
jedi2610 <krithickumarub@protonmail.com>
2021-04-03 15:22:00 +0530

    formatted code

--- config.h
+++ config.h
@@ -12,13 +12,12 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainMono Nerd Font:size=10", "Noto Color Emoji:size=10" };
-static const char dmenufont[]       = "JetBrainMono Nerd Font:size=10";
+static const char *fonts[]          = { "JetBrainMono Nerd Font:size=10", "Noto Color Emoji:pixelsize=12:antialias=true:autohint=true" };
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#BF616A";
+static const char col_cyan[]        = "#444444";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
